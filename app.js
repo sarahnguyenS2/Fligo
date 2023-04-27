b06bcfab0e364b09bff267037fe77fbc4abe992c
sarahnguyen <thanhhao2832@gmail.com>
2023-04-27 16:30:00 +0700

    Update user existed and encrypt password

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require("cors")
+const cors = require("cors");
+const bcrypt = require("bcryptjs")
 app.use(express.json());
+app.use(cors());
 
 const mongoUrl =
   "mongodb+srv://thanhhao:matkhau@cluster0.2qjoujj.mongodb.net/?retryWrites=true&w=majority";
@@ -22,7 +24,12 @@ const User = mongoose.model("UserInfo");
 
 app.post("/register", async (req, res) => {
   const { username, lname, fname, DOB, email, passportNo, password } = req.body;
+  const encryptedPassword = await bcrypt.hash(password, 10)
   try {
+    const oldUser = User.findOne({email})
+    if(oldUser) {
+        res.send({error: "User is existed!"})
+    }
     await User.create({
       username,
       lname,
@@ -30,7 +37,7 @@ app.post("/register", async (req, res) => {
       DOB,
       email,
       passportNo,
-      password,
+      password: encryptedPassword,
     });
     res.send({ status: "Ok" });
   } catch (error) {
